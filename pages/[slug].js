a794fa9570a80acf65282586db1388ea6ad715c8
Felipe César <felipecesr@gmail.com>
2020-04-02 23:25:10 -0300

    add syntax highlight

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,18 +1,28 @@
+import React, { useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
+import Prism from 'prismjs'
 
 import { getPostByFilename, getAllPaths } from '../lib/api'
 
 import { Layout } from '../layout'
 import { Bio } from '../components/Bio'
+import PrismStyles from '../styles/prism'
 
-const Post = ({ title, content }) => (
-  <Layout>
-    <h1>{title}</h1>
-    <ReactMarkdown source={content} />
-    <hr />
-    <Bio />
-  </Layout>
-)
+const Post = ({ title, content }) => {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [content])
+
+  return (
+    <Layout>
+      <PrismStyles />
+      <h1>{title}</h1>
+      <ReactMarkdown source={content} />
+      <hr />
+      <Bio />
+    </Layout>
+  )
+}
 
 export async function getStaticPaths() {
   const paths = getAllPaths()
