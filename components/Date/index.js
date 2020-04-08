d775ab23b7dfe573fe490545625279c5e3bcc8e2
Felipe César <felipecesr@gmail.com>
2020-04-07 22:34:35 -0300

    translate date

--- components/Date/index.js
+++ components/Date/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { parseISO, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import * as S from './styles'
 
 const Date = ({ dateString }) => {
   const date = parseISO(dateString)
   return (
     <S.DateWrapper dateTime={dateString}>
-      {format(date, 'LLLL	d, yyyy')}
+      {format(date, 'LLLL	d, yyyy', { locale: ptBR })}
     </S.DateWrapper>
   )
 }
