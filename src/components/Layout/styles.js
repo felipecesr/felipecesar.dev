c5661843e05b74a9bad143b94ae1f8708c964bb5
Felipe César <felipecesr@gmail.com>
2020-10-20 10:13:41 -0300

    fix: footer gradient

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -27,19 +27,24 @@ export const SkipLink = styled.a`
 export const Footer = styled.footer`
   position: relative;
   padding-top: 6rem;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    height: 490px;
+    z-index: -1;
+    background: linear-gradient(
+      180deg,
+      #1d2128 0%,
+      rgba(66, 81, 98, 0.36) 100%
+    );
+  }
 `;
 
 export const Wrapper = styled(Container)`
   padding-top: 2rem;
   padding-bottom: 2rem;
-`;
-
-export const Gradient = styled.div`
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
-  width: 100%;
-  height: 490px;
-  z-index: -1;
-  background: linear-gradient(180deg, #1d2128 0%, rgba(66, 81, 98, 0.36) 100%);
 `;
