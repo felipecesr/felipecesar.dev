98385a94431669ca5cca40f186194d31461e440f
Felipe César <felipecesr@gmail.com>
2020-10-20 22:11:26 -0300

    feat: add link in headings

--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -1,12 +1,16 @@
 import unified from "unified";
-import remarkParse from "remark-parse";
+import markdown from "remark-parse";
 import remarkRehype from "remark-rehype";
+const slug = require("remark-slug");
+const headings = require("remark-autolink-headings");
 import html from "rehype-stringify";
 import mdxPrism from "mdx-prism";
 
 export default async function markdownToHtml(markdown = "") {
   const result = await unified()
-    .use(remarkParse)
+    .use(markdown)
+    .use(slug)
+    .use(headings)
     .use(remarkRehype)
     .use(mdxPrism)
     .use(html)
