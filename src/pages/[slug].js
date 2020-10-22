39ff92ea6fdc6a440ef8d915021a4b5feffaa510
Felipe César <felipecesr@gmail.com>
2020-10-22 14:38:29 -0300

    feat: add style to logo

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -30,6 +30,16 @@ export const HeroInner = styled(Container)`
 export const Nav = styled.nav`
   display: block;
   margin-bottom: 2rem;
+
+  a {
+    text-decoration: none;
+    font-weight: 700;
+    font-size: 18px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 export default function Post({ slug, title, date, excerpt, content }) {
