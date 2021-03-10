bfe4804e737354ee835a87d57293da9dc81b31b7
Felipe César <felipecesr@gmail.com>
2021-03-10 17:51:03 -0300

    chore: change hero-inner-home to sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,23 @@
 import fs from "fs";
-import styled from "styled-components";
-import media from "styled-media-query";
 
 import heroStyles from "components/Hero.module.scss";
 import Main from "components/Main";
 import titleStyles from "components/Title.module.scss";
 import List from "components/List";
-import containerStyles from "components/Container.module.scss";
+import { container } from "components/Container.module.scss";
 import Social from "components/Social";
 import { getAllPosts } from "lib/api";
 import { generateRss } from "lib/rss";
-
-const HeroInner = styled.div`
-  padding-top: 6rem;
-  padding-bottom: 6rem;
-
-  ul {
-    margin-top: 2rem;
-  }
-
-  li {
-    font-size: 1.2rem;
-
-    ${media.greaterThan("medium")`
-      font-size: 1.333rem;
-    `}
-  }
-
-  ${media.greaterThan("medium")`
-    padding-top: 8rem;
-    padding-bottom: 8rem;
-  `}
-
-  ${media.greaterThan("large")`
-    padding-top: 10rem;
-    padding-bottom: 10rem;
-  `}
-
-  ${media.greaterThan("huge")`
-    padding-top: 13rem;
-    padding-bottom: 13rem;
-  `}
-`;
+import { heroInnerHome } from 'components/HeroInner.module.scss'
 
 export default function Index({ allPosts }) {
   return (
     <>
       <header className={heroStyles.hero}>
-        <HeroInner className={containerStyles.container}>
+        <div className={`${container} ${heroInnerHome}`}>
           <h1>Felipe César</h1>
           <Social />
-        </HeroInner>
+        </div>
       </header>
       <Main>
         <h2 className={titleStyles.title}>Últimos posts</h2>
