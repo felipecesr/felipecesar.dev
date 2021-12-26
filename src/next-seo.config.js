61f196ccec2455f75ee03c278f3ca151d4bf4999
Felipe César <felipecesr@gmail.com>
2021-12-26 12:26:13 -0300

    feat: add hugo theme

--- src/next-seo.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  title: "Felipe César - Desenvolvedor Front-End",
-  titleTemplate: "%s",
-  description: "Blog de um Desenvolvedor Front-End.",
-  twitter: {
-    site: "@felipecesr",
-  },
-};
