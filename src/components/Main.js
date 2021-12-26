5680b1e46acb220390af03f843a338d7015a754b
Felipe César <felipecesr@gmail.com>
2021-12-26 12:26:13 -0300

    feat: add hugo theme

--- src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import styles from "styles/components/Container.module.scss";
-
-export default function Main({ children }) {
-  return (
-    <main id="main">
-      <div className={styles.container + ' ' + styles['px-1']}>{children}</div>
-    </main>
-  );
-}
