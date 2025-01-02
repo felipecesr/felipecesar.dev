dc5edb05d0073a6f79f2ad3e5eeb68baa0f088c0
Felipe Cesar <felipecesr@gmail.com>
2025-01-02 14:22:20 -0300

    chore: change posts dir

--- src/app/posts/[slug]/utils.ts
+++ src/app/posts/[slug]/utils.ts
@@ -1,54 +1,54 @@
-import fs from 'node:fs'
-import path from "node:path"
+import fs from "node:fs";
+import path from "node:path";
 
 type Metadata = {
-  title: string
-  publishedAt: string
-  summary: string
-  image?: string
-}
+  title: string;
+  publishedAt: string;
+  summary: string;
+  image?: string;
+};
 
 function parseFrontmatter(fileContent: string) {
-  let frontmatterRegex = /---\s*([\s\S]*?)\s*---/
-  let match = frontmatterRegex.exec(fileContent)
-  let frontMatterBlock = match![1]
-  let content = fileContent.replace(frontmatterRegex, '').trim()
-  let frontMatterLines = frontMatterBlock.trim().split('\n')
-  let metadata: Partial<Metadata> = {}
+  let frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
+  let match = frontmatterRegex.exec(fileContent);
+  let frontMatterBlock = match![1];
+  let content = fileContent.replace(frontmatterRegex, "").trim();
+  let frontMatterLines = frontMatterBlock.trim().split("\n");
+  let metadata: Partial<Metadata> = {};
 
   frontMatterLines.forEach((line) => {
-    let [key, ...valueArr] = line.split(': ')
-    let value = valueArr.join(': ').trim()
-    value = value.replace(/^['"](.*)['"]$/, '$1') // Remove quotes
-    metadata[key.trim() as keyof Metadata] = value
-  })
+    let [key, ...valueArr] = line.split(": ");
+    let value = valueArr.join(": ").trim();
+    value = value.replace(/^['"](.*)['"]$/, "$1"); // Remove quotes
+    metadata[key.trim() as keyof Metadata] = value;
+  });
 
-  return { metadata: metadata as Metadata, content }
+  return { metadata: metadata as Metadata, content };
 }
 
 function getMDXFiles(dir) {
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === '.md')
+  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".md");
 }
 
 function readMDXFile(filePath) {
-  let rawContent = fs.readFileSync(filePath, 'utf-8')
-  return parseFrontmatter(rawContent)
+  let rawContent = fs.readFileSync(filePath, "utf-8");
+  return parseFrontmatter(rawContent);
 }
 
 function getMDXData(dir) {
-  let mdxFiles = getMDXFiles(dir)
+  let mdxFiles = getMDXFiles(dir);
   return mdxFiles.map((file) => {
-    let { metadata, content } = readMDXFile(path.join(dir, file))
-    let slug = path.basename(file, path.extname(file))
+    let { metadata, content } = readMDXFile(path.join(dir, file));
+    let slug = path.basename(file, path.extname(file));
 
     return {
       metadata,
       slug,
       content,
-    }
-  })
+    };
+  });
 }
 
 export function getBlogPosts() {
-  return getMDXData(path.join(process.cwd(), '_posts'))
+  return getMDXData(path.join(process.cwd(), "content/posts"));
 }
