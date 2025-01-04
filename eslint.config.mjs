6dab4652593370d4d11f248f861f535274011c58
Felipe Cesar <felipecesr@gmail.com>
2025-01-04 09:07:11 -0300

    temp: disable rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@next/next/no-img-element": "off",
+      "prefer-const": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
